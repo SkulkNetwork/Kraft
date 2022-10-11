5397d2b593ce56dbd05712267dbfa1c6a9c8cb5b
RGBCube <rgbcube@users.noreply.github.com>
2022-10-11 21:19:09 +0300

    Use standard project structure and add .toKraft to SpawnCategory

--- src/main/kotlin/tk/skulk/kraft/toKraft.kt
+++ src/main/kotlin/tk/skulk/kraft/toKraft.kt
@@ -4,7 +4,10 @@ import tk.skulk.kraft.player.KraftOfflinePlayer
 import tk.skulk.kraft.player.KraftPlayer
 import org.bukkit.OfflinePlayer as BukkitOfflinePlayer
 import org.bukkit.entity.Player as BukkitPlayer
+import org.bukkit.entity.SpawnCategory as BukkitSpawnCategory
 
 fun BukkitPlayer.toKraft() = KraftPlayer(this)
 
 fun BukkitOfflinePlayer.toKraft() = KraftOfflinePlayer(this)
+
+fun BukkitSpawnCategory.toKraft() = KraftSpawnCategory.valueOf(this.name)
